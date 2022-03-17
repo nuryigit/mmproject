fccecb8e7287b0ed9e4d6ca0285fc4f03891e071
fatma.yigit <fatma.yigit@etiya.com.tr>
2022-03-17 12:26:31 +0300

    Dependencies between modules
    mvn install on parent pom;
    web-app depend on jpa-entites & web-api because of this compiled last.
    [INFO] Reactor Build Order:
    [INFO]
    [INFO] mmproject                                                          [pom]
    [INFO] jpa-entities                                                       [jar]
    [INFO] from-intellij                                                      [jar]
    [INFO] web-api                                                            [jar]
    [INFO] web-app                                                            [jar]

---
+++ web-app/src/main/java/com/ny/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.ny.controllers;
+
+import com.ny.domain.UserCommand;
+import com.ny.entities.User;
+
+public class UserController {
+
+  User saveUser(UserCommand userCommand) {
+    //fake impl
+    return new User();
+  }
+}
